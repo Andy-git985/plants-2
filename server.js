5d395582417639364da195f6bd5ec142473c877e
Andy-git985 <andy.yu617@gmail.com>
2022-07-05 19:37:36 -0400

    ejs image rendering

--- server.js
+++ server.js
@@ -21,18 +21,18 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     // app.get('/', (req, res) => {
     //   res.send('Hello World');
     // });
-    app.get('/', (req, res) => {
-      res.sendFile(__dirname + '/index.html');
-    });
     // app.get('/', (req, res) => {
-    //   db.collection('store')
-    //     .find()
-    //     .toArray()
-    //     .then((store) => {
-    //       res.render('index.ejs', { store: store });
-    //     })
-    //     .catch(/* ... */);
+    //   res.sendFile(__dirname + '/index.html');
     // });
+    app.get('/', (req, res) => {
+      db.collection('store')
+        .find()
+        .toArray()
+        .then((store) => {
+          res.render('index.ejs', { store: store });
+        })
+        .catch(/* ... */);
+    });
 
     const PORT = process.env.PORT || 8000;
     app.listen(PORT, () => console.log(`Listening on ${PORT}`));
